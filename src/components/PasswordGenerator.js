ad750818f9f6352a926caf46100684f90a1beb8a
yashkumarsaurabh <65075425+yashkumarsaurabh@users.noreply.github.com>
2025-05-14 00:07:27 +0530

    password generator

---
+++ src/components/PasswordGenerator.js
@@ -0,0 +1,93 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
+function PasswordGenerator() {
+    const [password, setPassword] = useState("");
+    const [len, setLen] = useState(8);
+    const [numAllowed, setNumAllowed] = useState(false);
+    const [charAllowed, setCharAllowed] = useState(false);
+
+    const generatePassword = useCallback(() => {
+        let pass = '';
+        let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        if(numAllowed) str+='0123456789';
+        if(charAllowed) str+='~!@#$%^&*()';
+
+        for(let i=0; i<len; i++) {
+            let idx = Math.floor(Math.random()*str.length+1);
+            pass+=str.charAt(idx);
+        }
+
+        setPassword(pass);
+
+    }, [len, numAllowed, charAllowed, setPassword]);
+
+    useEffect(() => {
+        generatePassword();
+    }, [len, numAllowed, charAllowed]);
+
+    const passwordRef = useRef(null);
+
+    const copyPasswordToClipboard = useCallback(() => {
+        passwordRef.current?.select();
+        passwordRef.current?.setSelectionRange(0, 999);
+        window.navigator.clipboard.writeText(password)
+    }, [password]);
+
+    return (
+        <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
+      <h1 className='text-white text-center my-3'>Password generator</h1>
+    <div className="flex shadow rounded-lg overflow-hidden mb-4">
+        <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passwordRef}
+        />
+        <button
+        onClick={copyPasswordToClipboard}
+        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+        >copy</button>
+        
+    </div>
+    <div className='flex text-sm gap-x-2'>
+      <div className='flex items-center gap-x-1'>
+        <input 
+        type="range"
+        min={6}
+        max={100}
+        value={len}
+         className='cursor-pointer'
+         onChange={(e) => {setLen(e.target.value)}}
+          />
+          <label>Length: {len}</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+      <input
+          type="checkbox"
+          defaultChecked={numAllowed}
+          id="numberInput"
+          onChange={() => {
+              setNumAllowed((prev) => !prev);
+          }}
+      />
+      <label htmlFor="numberInput">Numbers</label>
+      </div>
+      <div className="flex items-center gap-x-1">
+          <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                  setCharAllowed((prev) => !prev )
+              }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+      </div>
+    </div>
+</div>
+    )
+}
+
+export default PasswordGenerator;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
-import BgChanger from './components/BgChanger';
+import PasswordGenerator from './components/PasswordGenerator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BgChanger />
+    <PasswordGenerator />
   </React.StrictMode>
 );
